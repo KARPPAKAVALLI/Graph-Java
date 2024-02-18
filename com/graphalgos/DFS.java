671ac296983d7e55f36fd5e66ffbd7de734f16f1
skarppakavalli@gmail.com <skarppakavalli@gmail.com>
2024-02-18 21:53:37 +0530

    "feat: completed DFS function code"

--- com/graphalgos/DFS.java
+++ com/graphalgos/DFS.java
@@ -1,7 +1,33 @@
+//this is a hard code
 package com.graphalgos;
 
-public class DFS {
-    public static void dfs(){
+import java.util.*;
 
+public class DFS {
+    public static void depthFirstSearch(Map<String, List<String>> graph , String start){
+        Stack<String> s=new Stack<>();
+        s.push(start);
+        while (!s.empty()){
+            String curr = s.pop();
+            System.out.println(curr);
+
+            List<String> neighbours = graph.get(curr); //graph.get(curr) -> is actually a list
+
+            for (String n : neighbours) {
+                s.push(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, List<String>> graph = new HashMap<>();
+        graph.put("a", List.of("c", "b"));
+        graph.put("b", List.of("d"));
+        graph.put("c", List.of("e"));
+        graph.put("d", List.of("f"));
+        graph.put("e", List.of());
+        graph.put("f", List.of());
+
+        depthFirstSearch(graph,"a");
     }
 }
