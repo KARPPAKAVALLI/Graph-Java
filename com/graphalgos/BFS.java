a7ad583761650a54c8195974c96e828b4302992e
skarppakavalli@gmail.com <skarppakavalli@gmail.com>
2024-02-19 19:49:54 +0530

    "feat: completed BFS code"

---
+++ com/graphalgos/BFS.java
@@ -0,0 +1,33 @@
+package com.graphalgos;
+
+import java.util.*;
+
+//Similar, to DFS Code but instead of Stack use Queue
+public class BFS {
+    public static void breadthFirstSearch(Map<String, List<String>> graph , String start){
+        Queue<String> q=new LinkedList<>();
+        q.add(start);
+        while (!q.isEmpty()){
+            String curr = q.remove();
+            System.out.println(curr);
+
+            List<String> neighbours = graph.get(curr); //graph.get(curr) -> is actually a list
+
+            for (String n : neighbours) {
+                q.add(n);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, List<String>> graph = new HashMap<>();
+        graph.put("a", List.of("c", "b"));
+        graph.put("b", List.of("d"));
+        graph.put("c", List.of("e"));
+        graph.put("d", List.of("f"));
+        graph.put("e", List.of());
+        graph.put("f", List.of());
+
+        breadthFirstSearch(graph,"a");
+    }
+}
